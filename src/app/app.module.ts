b424bba362454b383e66b022552419bf8d489ddd
Francisco Vaz <ftomasvaz@gmail.com>
2020-12-31 11:13:13 +0000

    add name to users route and fetching route parameters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { ServersService } from './servers/servers.service';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UserComponent },
-  { path: 'users/:id', component: UserComponent },
+  { path: 'users/:id/:name', component: UserComponent },
   { path: 'servers', component: ServersComponent }
 ];
 @NgModule({
